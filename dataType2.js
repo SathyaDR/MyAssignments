7767cbb96248eb03e88ea9e072cbd1a9bf107186
SathyaDR <sathyabamar@gmail.com>
2025-10-20 22:51:03 +0530

    Add files via upload
    dataType2

---
+++ dataType2.js
@@ -0,0 +1,11 @@
+var empDetails = "sathya";
+empDetails = 2000;
+var empDetails  = true;
+console.log(empDetails);
+
+let studentInfo = 2001;
+studentInfo = null;
+console.log(studentInfo);
+
+const piValue = 222.344;
+console.log(piValue);
