558c0c8ec2addea6e773959f7a3afc9d8569ab4d
SathyaDR <sathyabamar@gmail.com>
2025-10-20 22:47:55 +0530

    Add files via upload
    dataType2

---
+++ conditionalStatement.js
@@ -0,0 +1,9 @@
+let mark = 50;
+if(mark>70){
+console.log("pass : Mark is greater than 70");
+}else if (mark >55) {
+console.log("pass : Mark is greater than 55");
+} else if(mark > 45) {
+console.log("pass : Mark is greater than 45");
+} else { console.log("fail");
+}
