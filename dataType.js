424b6fdbd3ec6d035cee1cb7763ed2d3dc288ef5
SathyaDR <sathyabamar@gmail.com>
2025-10-20 22:45:16 +0530

    Add files via upload
    Week1-Day1

---
+++ dataType.js
@@ -0,0 +1,10 @@
+var firstName = 'sathya';
+var companyName = "TestLeaf";
+var mobileNumber = 23472462;
+var isautomation = true
+var hasPlaywright
+console.log("FirstName:", typeof(firstName));
+console.log("CompanyName:", typeof(companyName));
+console.log("MobileNumber:", typeof(mobileNumber));
+console.log("Datatype:"+ typeof (isautomation));
+console.log("Datatype:"+ typeof (hasPlaywright));
